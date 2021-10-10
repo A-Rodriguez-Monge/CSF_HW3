0be596cfc7b5f2818845ce327742d75b7d72e2fb
Alejandro <a12monge50@gmail.com>
2021-10-10 19:11:20 -0400

    added main, added to makefile

--- Makefile
+++ Makefile
@@ -3,8 +3,12 @@ CONSERVATIVE_FLAGS = -std=c++11 -Wall -Wextra -pedantic
 DEBUGGING_FLAGS = -g
 CFLAGS = $(CONSERVATIVE_FLAGS) $(DEBUGGING_FLAGS)
 
+csim: main.o
+	$(CC) $(CFLAGS) main.o -o csim
+
 main.o: main.cpp
 	$(CC) $(CFLAGS) -c main.cpp
 
 clean:
 	rm -f *.o main
+	rm -f csim
---
+++ main.cpp
@@ -0,0 +1,20 @@
+//Alejandro Rodriguez - arodri75
+//Pamela Li           - pli36
+
+#include <cstdio>
+
+/*
+  - number of sets in the cache (a positive power 2)
+  - number of blocks in each set (a positive power of 2)
+  - number of bytes in each block (a positive power of 2, at least 4)
+  - write-allocate or no-write-allocate
+  - write-through or write-back
+  - lru (least recently used) or fifo evictions
+*/
+
+
+int main(int argc, char **argv) {
+
+  printf("hello\n");
+  return 0;
+}
