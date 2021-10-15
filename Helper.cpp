8fbfba6593b959f41b774e2bec4788316f032fb8
Alejandro <a12monge50@gmail.com>
2021-10-15 11:04:43 -0400

    added Helper file

---
+++ Helper.cpp
@@ -0,0 +1,66 @@
+#include <cstdio>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include "Helper.h"
+
+using std::cerr;
+using std::isdigit;
+
+int checkArgs(int argc, char **argv) {
+
+  //check num args
+  if (argc != 7) {
+    cerr << "Invalid Input: Incorrect Number of Arguments\n";
+    exit(1);
+  }
+
+  //check args 1-4
+  for (int i = 1; i < 4; i++) {
+    if((isdigit(argv[i] != 0) || (atoi(argv[i]) <= 0))) {
+      cerr << "Invalid Input: values must be positive\n";
+      exit(1);
+    }
+
+    if ((i == 3) && (atoi(argv[i]) < 4)) {
+      cerr << "Invalid Input: number of bytes must be at least 4\n";
+      exit(1);
+    }
+  }
+
+  //check args 4-6
+  if ((strcmp("write-allocate", argv[4]) != 0) && (strcmp("no-write-allocate", argv[4]) != 0)) {
+      cerr << "Invalid Input: 4th argument is invalid\n";
+      exit(1);
+  }
+
+  if ((strcmp("write-through", argv[5]) !=0) && (strcmp("write-back", argv[5]) != 0)) {
+      cerr << "Invalid Input: 5th argument is invalid\n";
+      exit(1);
+  }
+
+  if ((strcmp("lru", argv[6]) !=0) && (strcmp("fifo", argv[6]) != 0)) {
+      cerr << "Invalid Input: 5th argument is invalid - must be lru or  fifo\n";
+      exit(1);
+  }
+
+  //validate arg combos
+  if (strcmp("write-through", argv[5]) == 0) {
+    if (strcmp("no-write-allocate", argv[4]) != 0) {
+      cerr << "Invalid Combo\n";
+      exit(1);
+    }
+  }
+
+  if (strcmp("write-back", argv[5]) == 0) {
+    if (strcmp("write-allocate", argv[4]) != 0) {
+      cerr << "Invalid Combo\n";
+      exit(1);
+    }
+  }
+
+  return 0;
+
+}
---
+++ Helper.h
@@ -0,0 +1,6 @@
+#ifndef _HELPER_H
+#define _HELPER_H
+
+int checkArgs(int argc, char **argv);
+
+#endif
